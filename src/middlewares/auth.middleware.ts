f716fb9df348ac75257bec2fedd3af4ebfbe1efb
Shreyas M S <shreyasmadappady20@gmail.com>
2025-06-11 02:07:45 +0530

    c

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,38 +1,25 @@
 import { Request, Response, NextFunction } from "express";
-import { verifyToken } from "../utils/jwt";
-import prisma from "../config/prisma";
+import admin from "../firebase/firebaseAdmin";
 
-interface AuthRequest extends Request {
-  user?: {
-    id: string;
-  };
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
 }
 
-export const auth = async (
-  req: AuthRequest,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.header("Authorization")?.replace("Bearer ", "");
-
     if (!token) {
       res.status(401).json({ error: "Please authenticate" });
       return;
     }
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = { id: decodedToken.uid, ...decodedToken };
+    console.log(`decodedToken`, req.user);
 
-    const decoded = verifyToken(token);
-
-    const user = await prisma.user.findUnique({
-      where: { id: decoded.id },
-    });
-
-    if (!user) {
-      res.status(401).json({ error: "Please authenticate" });
-      return;
-    }
-
-    req.user = { id: user.id };
     next();
   } catch (error) {
     res.status(401).json({ error: "Please authenticate" });
